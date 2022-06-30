f0cfdc3be35994d19e98ba39afbb9d7e5d7b9349
swetavsavarn <33478489+swetavsavarn@users.noreply.github.com>
2022-06-30 17:48:36 +0530

    Create second assingemnet.js

---
+++ second assingemnet.js
@@ -0,0 +1,8 @@
+
+let age=21;
+let address ="abc";
+let name1="def";
+let personalDetails=`My name is ${name1},age is ${age} , address is ${address}`;
+
+console.log(personalDetails)
+console.log(personalDetails.split(','));
