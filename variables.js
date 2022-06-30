667217ff1757588d79ea194be2bea35039a6b084
swetavsavarn <33478489+swetavsavarn@users.noreply.github.com>
2022-06-30 17:50:04 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,14 @@
+//js has commonly three types of data types i.e. var,let and const. 
+//var is used when we have define global variables
+//let is used to define and declare variable around the scope
+//const value is defined and intialize at once it cant be reintialized
+var i=20;
+let j=30;
+const k=40;
+const l="swetav";
+const m=true;
+const n=null;
+const o=undefined;
+console.log(i,j,k);
+//typeof() is used to get the type of variables
+console.log(typeof i,typeof j,typeof k,typeof l,typeof m, typeof n,typeof o );
