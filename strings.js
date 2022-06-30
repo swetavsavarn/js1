15cfa7b9b4d972b6be446e4e3932cba884fbd9e4
swetavsavarn <33478489+swetavsavarn@users.noreply.github.com>
2022-06-30 17:48:52 +0530

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,11 @@
+const s1="swetav";
+const age1=21;
+//string concatination and methods
+//1st method
+console.log("my name is "+s1 + "my name is "+age);
+//string methods in js
+console.log(s1.toUpperCase());
+console.log(s1.length)
+console.log(s1.substring(0,2));
+console.log(s1.toLowerCase());
+console.log(s1.split('e'));
