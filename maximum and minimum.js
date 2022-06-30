0dc119d19f081dbd989add43f173be248ca929c1
swetavsavarn <33478489+swetavsavarn@users.noreply.github.com>
2022-06-30 17:45:32 +0530

    Create maximum and minimum.js

---
+++ maximum and minimum.js
@@ -0,0 +1,10 @@
+function abc(num1,num2)
+{
+    return num1+num2;
+}
+console.log(abc(2,3));
+
+//also we can convert these to skeltion function
+
+const def=(num3,num4)=>num3+num4;
+console.log(def(5,6));
